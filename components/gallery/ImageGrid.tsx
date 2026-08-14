375da028de6a09e139c2134d0de4af5494285245
mujtaba214 <mujtabausman21@gmail.com>
2026-08-14 17:28:11 +0500

    updated

--- components/gallery/ImageGrid.tsx
+++ components/gallery/ImageGrid.tsx
@@ -94,7 +94,7 @@ export function ImageGrid() {
         </motion.div>
 
         {/* 🔥 FIXED GRID - Responsive columns */}
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-5 lg:gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-5 lg:gap-6">
           {images.map((image, idx) => (
             <ImageCard
               key={`${image.id}-${idx}`}
