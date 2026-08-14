ac2dab1471d62c616776c62eb21788fe64bcc039
mujtaba214 <mujtabausman21@gmail.com>
2026-08-14 11:30:20 +0500

    updated

--- types/index.ts
+++ types/index.ts
@@ -10,17 +10,17 @@ export interface ImageType {
   createdAt: Date;
 }
 
-export const SAMPLE_IMAGES: ImageType[] = [
-  {
-    id: '1',
-    url: 'https://picsum.photos/id/1/800/800',  // Working URL
-    likes: 123,
-    downloads: 45,
-    views: 1234,
-    createdAt: new Date(),
-  },
-  // ... rest of images
-];
+// export const SAMPLE_IMAGES: ImageType[] = [
+//   {
+//     id: '1',
+//     url: 'https://picsum.photos/id/1/800/800',  // Working URL
+//     likes: 123,
+//     downloads: 45,
+//     views: 1234,
+//     createdAt: new Date(),
+//   },
+//   // ... rest of images
+// ];
 
 export interface UserInteraction {
   imageId: string;
